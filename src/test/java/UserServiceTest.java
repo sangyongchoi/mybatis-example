134de743461569924c8899fd262705e4b927fb49
sangyongchoi <csy7792@naver.com>
2021-03-07 18:13:14 +0900

    modify

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -9,14 +9,14 @@ class UserServiceTest {
 
     @Test
     @DisplayName("Insert 테스트")
-    public void insert_test() throws Exception{
+    public void insert_test() {
         UserService userService = new UserService();
         userService.insertUser();
     }
 
     @Test
     @DisplayName("Select Test")
-    public void select_test() throws Exception{
+    public void select_test() {
         UserService userService = new UserService();
         final List<User> users = userService.selectUser();
         users.forEach(System.out::println);
